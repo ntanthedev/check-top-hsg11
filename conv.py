8b02da0b57a8d3710e538be1eaabe47e4542b5e5
ntan <nhattanff07@gmail.com>
2025-03-21 16:34:15 +0700

    ok

--- conv.py
+++ conv.py
@@ -5,7 +5,7 @@ import json
 workbook = load_workbook(filename="uwu.xlsx", read_only=True)
 sheet = workbook.active
 
-headers = ["sbd", "ten", "ngay_sinh", "que_quan", "", "", "truong", "phong_gddt", "mon_thi", "diem", "ket_qua", "xep_giai"]
+headers = ["sbd", "ten", "ngay_sinh", "noi_sinh", "gioi_tinh", "lop", "truong", "mon_thi", "diem", "ket_qua", "xep_giai"]
 
 data = []
 for row in sheet.iter_rows():
@@ -18,11 +18,13 @@ for row in sheet.iter_rows():
 sanitized = {}
 
 for dat in data:
+    if dat["diem"] == 'Vắng thi':
+        dat["diem"] = 0
     dat["diem"] = float(dat["diem"])
     sanitized[dat["sbd"]] = dat
     del sanitized[dat["sbd"]]["sbd"]
 
-with open("check-top-hsg9-hatinh/data.json", 'w', encoding='utf-8') as f:
+with open("data.json", 'w', encoding='utf-8') as f:
     json.dump(sanitized, f, indent=4, ensure_ascii=False)
 
 workbook.close()
